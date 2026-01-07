251aa93724cc8f2f292c0f05bbbb76d681083578
hafiz abdullah <habdullah4510@gmail.com>
2026-01-08 00:46:33 +0500

    Implement socket.io client with connection management and event handling

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import cors from "cors";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+
+app.use(cors());
+
+const io = new Server(httpServer, {
+  cors: {
+    // origin: "http://localhost:3000",
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   socket.on("connect", () => {
     console.log("Connected succesfully.");
+  });
+
+  socket.on("create-something", (value) => {
+    console.log("Something:", value);
+    io.emit("foo", value);
   });
 
   socket.on("disconnect", () => {
@@ -22,6 +36,6 @@ app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-app.listen(8000, () => {
+httpServer.listen(8000, () => {
   console.log("Server connected successfuly.");
 });
